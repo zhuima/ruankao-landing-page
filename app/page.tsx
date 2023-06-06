57b13a1d8c146bba5fece6e12511ca6e5b3aded9
zhuima <zhuima314@gmail.com>
2023-06-06 15:52:37 +0800

    change the hero text

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Contact from "./components/contact";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <HeroSection />
       <Content />
       <Feature />
