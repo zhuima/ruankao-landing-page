50de6b2296704f872671195068c5a912d0ecfc68
zhuima <zhuima314@gmail.com>
2023-07-04 12:03:34 +0800

    add nprograss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import ScrollToTop from "./components/ScrollToTopButton";
@@ -9,6 +10,7 @@ export const metadata = {
   title: "软考通关宝典",
   description:
     "软考通关宝典提供掘金高效率工具计划、倒计时页面、信息系统项目管理师、系统集成项目管理工程师、系统规划与管理师、网络工程师、信息安全工程师等考试的在线培训、考试资讯、免费知识库等考试服务，是一个拥有独立创新的专业学习网站。",
+  keywords: ["软考", "软考通关宝典", "软考报名资料"],
 };
 
 export default function RootLayout({
@@ -19,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NextTopLoader />
         <Header />
         {children}
         <Footer />
