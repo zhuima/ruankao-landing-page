cbfa6d5aa3014ace0734b9f1005e0f766265819d
zhuima <zhuima314@gmail.com>
2023-07-07 16:39:57 +0800

    add sitemap and robots

---
+++ app/sitemap.tsx
@@ -0,0 +1,33 @@
+/*
+ * @Author: zhuima zhuima314@gmail.com
+ * @Date: 2023-07-07 16:33:57
+ * @LastEditors: zhuima zhuima314@gmail.com
+ * @LastEditTime: 2023-07-07 16:35:25
+ * @FilePath: /ruankao-website/app/sitemap.tsx
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
+// app/sitemap.js
+// https://claritydev.net/blog/nextjs-dynamic-sitemap-pages-app-directory
+
+const URL = "https://ruankao.eu.org";
+
+export default async function sitemap() {
+  const routes = [
+    "/",
+    "/cities",
+    "/countdown",
+    "/docs",
+    "/docs/about-ruankao",
+    "/docs/about-ruankao-certificates",
+    "/docs/high-level",
+    "/docs/high-level/xxxtxmgls",
+    "/docs/high-level/xtghygls",
+  ].map((route) => ({
+    url: `${URL}${route}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  return [...routes];
+}
--- components/mdx-card.tsx
+++ components/mdx-card.tsx
@@ -31,6 +31,22 @@ export function MdxCard({
       {href && (
         <Link href={disabled ? "#" : href} className="absolute inset-0">
           <span className="sr-only">View</span>
+          {/* <span className="mx-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+              ></path>
+            </svg>
+          </span> */}
         </Link>
       )}
     </div>
