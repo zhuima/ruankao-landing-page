4af6359e67ae5c80dd4f43deb6a5b94c1a02fa39
zhuima <zhuima314@gmail.com>
2023-09-01 11:24:31 +0800

    change keyword

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
  * @Author: zhuima zhuima314@gmail.com
  * @Date: 2023-07-06 15:11:36
  * @LastEditors: zhuima zhuima314@gmail.com
- * @LastEditTime: 2023-07-28 14:05:20
+ * @LastEditTime: 2023-09-01 11:23:49
  * @FilePath: /ruankao-website/app/layout.tsx
  * @Description:
  *
@@ -22,13 +22,14 @@ export const metadata = {
   description:
     "软考通关宝典提供掘金高效率工具计划、倒计时页面、信息系统项目管理师、系统集成项目管理工程师、系统规划与管理师、网络工程师、信息安全工程师等考试的在线培训、考试资讯、免费知识库等考试服务，是一个拥有独立创新的专业学习网站。",
   keywords: [
+    "软考",
+    "软考通关",
     "软考报名入口官网",
     "软考成绩查询",
     "软考官网",
     "软考报名",
     "网络工程师",
     "计算机软考",
-    "软考",
     "软考通关宝典",
     "软考报名资料",
   ],
