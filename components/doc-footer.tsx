5e50508da4fa313a0203495e8bf15d609190b627
zhuima <zhuima314@gmail.com>
2023-09-01 11:09:58 +0800

    change video page size

--- components/doc-footer.tsx
+++ components/doc-footer.tsx
@@ -2,7 +2,7 @@
  * @Author: zhuima zhuima314@gmail.com
  * @Date: 2023-07-24 10:34:32
  * @LastEditors: zhuima zhuima314@gmail.com
- * @LastEditTime: 2023-09-01 10:45:39
+ * @LastEditTime: 2023-09-01 11:09:46
  * @FilePath: /ruankao-website/components/doc-footer.tsx
  * @Description:
  *
@@ -42,7 +42,7 @@ export default function DocFooter({ pageTitle }) {
             </div>
           </div>
           <div className="flex-1 btn-base text-base text-muted-foreground btn-md sm:btn-lg bg-white  border dark:border-black border-neutral-200 dark:hover:border-neutral-800  sm:hover:bg-neutral-200 relative transition-colors">
-            <CopyPageLink text={pageTitle} />
+            <CopyPageLink pageTitle={pageTitle} />
             <div className="flex gap-2 justify-center items-center">
               <IoLink />
               复制链接
