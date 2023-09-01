0c5a631272a0932154ba42f573b8ff6090ef3189
zhuima <zhuima314@gmail.com>
2023-09-01 16:10:07 +0800

    change copy button action

--- components/doc-footer.tsx
+++ components/doc-footer.tsx
@@ -2,7 +2,7 @@
  * @Author: zhuima zhuima314@gmail.com
  * @Date: 2023-07-24 10:34:32
  * @LastEditors: zhuima zhuima314@gmail.com
- * @LastEditTime: 2023-09-01 15:56:50
+ * @LastEditTime: 2023-09-01 16:09:52
  * @FilePath: /ruankao-website/components/doc-footer.tsx
  * @Description:
  *
@@ -48,10 +48,13 @@ export default function DocFooter({ pageTitle }) {
               联系作者
             </div>
           </div>
-          <div className="flex-1 btn-base text-base text-muted-foreground btn-md sm:btn-lg bg-white  border dark:border-black border-neutral-200 dark:hover:border-neutral-800  sm:hover:bg-neutral-200 relative transition-colors">
+          <div
+            onClick={() => copyButtonToast()}
+            className="flex-1 btn-base text-base text-muted-foreground btn-md sm:btn-lg bg-white  border dark:border-black border-neutral-200 dark:hover:border-neutral-800  sm:hover:bg-neutral-200 relative transition-colors"
+          >
             <CopyPageLink pageTitle={pageTitle} />
             <div className="flex gap-2 justify-center items-center">
-              <IoLink onClick={() => copyButtonToast()} />
+              <IoLink />
               复制链接
             </div>
           </div>
