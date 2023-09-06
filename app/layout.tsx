8cd0d1000a13103c6f95325fd36649f51e7aab74
zhuima <zhuima314@gmail.com>
2023-09-06 14:13:40 +0800

    add banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
  * @Author: zhuima zhuima314@gmail.com
  * @Date: 2023-07-06 15:11:36
  * @LastEditors: zhuima zhuima314@gmail.com
- * @LastEditTime: 2023-09-01 11:23:49
+ * @LastEditTime: 2023-09-06 14:08:34
  * @FilePath: /ruankao-website/app/layout.tsx
  * @Description:
  *
@@ -12,9 +12,10 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import NextTopLoader from "nextjs-toploader";
 import { Analytics } from "@vercel/analytics/react";
-import Header from "../components/header";
-import Footer from "../components/footer";
-import ScrollToTop from "../components/ScrollToTopButton";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import ScrollToTop from "@/components/ScrollToTopButton";
+import StickyBanner from "@/components/stickybanner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -49,6 +50,7 @@ export default function RootLayout({
   return (
     <html lang="zh-CN">
       <body className={inter.className}>
+        <StickyBanner />
         <NextTopLoader />
         <Header />
         {children}
