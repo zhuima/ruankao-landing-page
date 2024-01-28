1a875ddd7e3f3511c6ff68919abbe4e9d21c5183
zhuima <zhuima314@gmail.com>
2024-01-28 13:46:03 +0800

    disable hero section overflow

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -151,22 +151,21 @@ const HeroSection = () => {
               />
             </div> */}
           </div>
-          <div className="mt-2 md:mt-0 lg:absolute -right-10 lg:w-7/12">
-            <div className="relative w-full overflow-hidden">
+          <div className="mt-2 md:mt-0 lg:absolute -right-10 lg:w-7/12 ">
+            <div className="relative w-full ">
               <div
                 aria-hidden="true"
                 className="absolute scale-75 md:scale-110 inset-0 m-auto w-full h-full md:w-96 md:h-96 rounded-full rotate-45 bg-gradient-to-r from-sky-500 to-cyan-300 blur-3xl"
-              >
-                <Image
-                  src="https://tailus.io/sources/blocks/tech-startup/preview/images/globalization-cuate.svg"
-                  className="relative w-full"
-                  alt="wath illustration"
-                  width={320}
-                  height={480}
-                  style={{ height: "480px", width: "100%" }}
-                  priority={true}
-                />
-              </div>
+              ></div>
+              <Image
+                src="https://tailus.io/sources/blocks/tech-startup/preview/images/globalization-cuate.svg"
+                className="relative w-full"
+                alt="wath illustration"
+                width={320}
+                height={480}
+                style={{ height: "480px", width: "100%" }}
+                priority={true}
+              />
             </div>
           </div>
         </div>
