dea33fc478e6636bf68e6684b1b85984de70594e
zhuima <zhuima314@gmail.com>
2024-01-28 16:55:48 +0800

    disable scrolling section overflow for mobile

--- components/scrolling.tsx
+++ components/scrolling.tsx
@@ -26,161 +26,149 @@ const Scrolling = () => {
         <div className="relative flex flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
           <div className="pointer-events-none relative flex flex-nowrap gap-10 overflow-hidden">
             <div className="flex flex-wrap min-w-full shrink-0 animate-marquee items-center justify-around gap-10">
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/19d147f36f366c0ad9641.png"
-                alt=""
-                height="404"
-                width="640"
-              />
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/fa44e9283405c2a24cb8d.png"
-                alt=""
-                height="404"
-                width="640"
-              />
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/a5f640f6d8f5f508f6529.png"
-                alt=""
-                height="404"
-                width="640"
-              />
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/3398460f7e183e4459209.png"
-                alt=""
-                height="404"
-                width="640"
-              />
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/31eb6faaf35812d30bcc0.png"
-                alt=""
-                height="404"
-                width="640"
-              />
+              <div className="aspect-square max-w-[clamp(10rem,28vmin,20rem)] rounded-md object-cover shadow-md">
+                <Image
+                  src="https://img.techrk1688.eu.org/file/19d147f36f366c0ad9641.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+                <Image
+                  src="https://img.techrk1688.eu.org/file/fa44e9283405c2a24cb8d.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+                <Image
+                  src="https://img.techrk1688.eu.org/file/a5f640f6d8f5f508f6529.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+                <Image
+                  src="https://img.techrk1688.eu.org/file/3398460f7e183e4459209.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+                <Image
+                  src="https://img.techrk1688.eu.org/file/31eb6faaf35812d30bcc0.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+              </div>
             </div>
             <div
               aria-hidden="true"
               className="flex flex-wrap min-w-full shrink-0 animate-marquee items-center justify-around gap-10"
             >
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/19d147f36f366c0ad9641.png"
-                alt=""
-                height="404"
-                width="640"
-              />
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/fa44e9283405c2a24cb8d.png"
-                alt=""
-                height="404"
-                width="640"
-              />
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/a5f640f6d8f5f508f6529.png"
-                alt=""
-                height="404"
-                width="640"
-              />
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/3398460f7e183e4459209.png"
-                alt=""
-                height="404"
-                width="640"
-              />
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/31eb6faaf35812d30bcc0.png"
-                alt=""
-                height="404"
-                width="640"
-              />
+              <div className="aspect-square max-w-[clamp(10rem,28vmin,20rem)] rounded-md object-cover shadow-md">
+                <Image
+                  src="https://img.techrk1688.eu.org/file/19d147f36f366c0ad9641.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+                <Image
+                  src="https://img.techrk1688.eu.org/file/fa44e9283405c2a24cb8d.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+                <Image
+                  src="https://img.techrk1688.eu.org/file/a5f640f6d8f5f508f6529.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+                <Image
+                  src="https://img.techrk1688.eu.org/file/3398460f7e183e4459209.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+                <Image
+                  src="https://img.techrk1688.eu.org/file/31eb6faaf35812d30bcc0.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+              </div>
             </div>
           </div>
 
           <div className="pointer-events-none relative mt-10 flex flex-nowrap gap-10 overflow-hidden">
             <div className="flex flex-wrap min-w-full shrink-0 animate-marquee items-center justify-around gap-10 [animation-direction:reverse]">
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/aef373fcfbddeb1529cce.png"
-                alt=""
-                height="404"
-                width="640"
-              />
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/ecba580f0e69736dcede9.png"
-                alt=""
-                height="404"
-                width="640"
-              />
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/9465a970a407df9abac87.png"
-                alt=""
-                height="404"
-                width="640"
-              />
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/e72924904810aaea58025.png"
-                alt=""
-                height="404"
-                width="640"
-              />
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/19d147f36f366c0ad9641.png"
-                alt=""
-                height="404"
-                width="640"
-              />
+              <div className="aspect-square max-w-[clamp(10rem,28vmin,20rem)] rounded-md object-cover shadow-md">
+                <Image
+                  src="https://img.techrk1688.eu.org/file/aef373fcfbddeb1529cce.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+                <Image
+                  src="https://img.techrk1688.eu.org/file/ecba580f0e69736dcede9.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+                <Image
+                  src="https://img.techrk1688.eu.org/file/9465a970a407df9abac87.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+                <Image
+                  src="https://img.techrk1688.eu.org/file/e72924904810aaea58025.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+                <Image
+                  src="https://img.techrk1688.eu.org/file/19d147f36f366c0ad9641.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+              </div>
             </div>
             <div
               aria-hidden="true"
               className="flex flex-wrap min-w-full shrink-0 animate-marquee items-center justify-around gap-10 [animation-direction:reverse]"
             >
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/aef373fcfbddeb1529cce.png"
-                alt=""
-                height="404"
-                width="640"
-              />
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/ecba580f0e69736dcede9.png"
-                alt=""
-                height="404"
-                width="640"
-              />
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/9465a970a407df9abac87.png"
-                alt=""
-                height="404"
-                width="640"
-              />
-              <Image
-                className="aspect-square max-w-full rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/e72924904810aaea58025.png"
-                alt=""
-                height="404"
-                width="640"
-              />
-              <Image
-                className="aspect-square max-w-full  rounded-md object-cover shadow-md"
-                src="https://img.techrk1688.eu.org/file/19d147f36f366c0ad9641.png"
-                alt=""
-                height="404"
-                width="640"
-              />
+              <div className="aspect-square max-w-[clamp(10rem,28vmin,20rem)] rounded-md object-cover shadow-md">
+                <Image
+                  src="https://img.techrk1688.eu.org/file/aef373fcfbddeb1529cce.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+                <Image
+                  src="https://img.techrk1688.eu.org/file/ecba580f0e69736dcede9.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+                <Image
+                  src="https://img.techrk1688.eu.org/file/9465a970a407df9abac87.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+                <Image
+                  src="https://img.techrk1688.eu.org/file/e72924904810aaea58025.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+                <Image
+                  src="https://img.techrk1688.eu.org/file/19d147f36f366c0ad9641.png"
+                  alt=""
+                  height="404"
+                  width="640"
+                />
+              </div>
             </div>
           </div>
         </div>
